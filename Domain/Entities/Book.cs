8d98e86f5301fca814ec41f086ffd01537e8afee
Mark Denysenko <markentos@mail.ru>
2018-09-22 22:03:13 +0300

    First working prototype without DB

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Book
+    {
+        public int BookId { set; get; }
+        public string Name { set; get; }
+        public string Author { set; get; }
+        public string Descriprion { set; get; }
+        public string Genre { set; get; }
+        public decimal Price { set; get; }
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,54 @@
+﻿using Ninject.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Domain.Abstract;
+using Moq;
+using Domain.Entities;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver, IHaveKernel
+    {
+        private IKernel kernel;
+        public IKernel Kernel
+        {
+            get
+            {
+                return kernel;
+            }
+        }
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            // привязка данных
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book {Name = "Book1", Author = "Author1", Price = 110},
+                new Book {Name = "Book2", Author = "Author2", Price = 50},
+                new Book {Name = "Book3", Author = "Author3", Price = 200},
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
